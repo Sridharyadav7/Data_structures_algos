7f49f9ddf73dcccd5963f9ba5b2ebbb28b16a5c3
SridharYadav <sridharyadav2003@gmail.com>
2024-07-24 11:53:00 +0530

    Initial commit

---
+++ Linearsearch.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Linearsearch {
+    public static void main(String[] args)
+    {
+        int[][] items=new int[3][3];
+        Scanner sc= new Scanner(System.in);
+
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                items[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Enter the item to be searched: ");
+        int key=sc.nextInt();
+        Linear a=new Linear();
+        int[] result=a.check(items,key);
+        if(result[0]==1)
+        {
+            System.out.println("Item found at position "+result[1]+","+result[2]);
+        }
+        else{
+            System.out.println("Item not found");
+        }
+       
+}}
+
+class Linear
+{
+    int[] check(int[][] items, int key)
+    {
+        for(int i=0;i<items.length;i++)
+        {
+            for(int j=0;j<items[i].length;j++)
+            {
+                if(items[i][j]==key)
+                 {
+                       return new int[]{1,i,j};
+                 }
+            }
+        }
+        return new int[]{0,-1,-1};
+    }
+}
