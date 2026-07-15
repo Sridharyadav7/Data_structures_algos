fb0ef177941b37f1685230670e3c0e1e737e8cb9
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-07-15 19:17:05 +0530

    Time: 15 ms (65.28%), Space: 47.5 MB (79.41%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -9,7 +9,7 @@ class Solution {
         for(int arr[]: res) Arrays.fill(arr, Integer.MAX_VALUE);
         res[0][0] = 1;
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.dis - b.dis);
+        Queue<Pair> pq = new LinkedList<>();
         pq.add(new Pair(0, 0, 1));
 
         while(!pq.isEmpty()) {
@@ -23,12 +23,9 @@ class Solution {
                 int nr = row + deltaRow[i];
                 int nc = col + deltaCol[i];
 
-                if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 0) {
-                    int newDis = dis + 1;
-                    if(newDis < res[nr][nc]) {
-                        res[nr][nc] = newDis;
-                        pq.add(new Pair(nr, nc, newDis));
-                    }
+                if(nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 0 && res[nr][nc] ==  Integer.MAX_VALUE) {
+                    res[nr][nc] = dis + 1;
+                    pq.add(new Pair(nr, nc, dis + 1));
                 }
             }
         }
