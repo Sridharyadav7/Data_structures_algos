54cc626cf069bf84c78e55702f633301c6c4199d
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-08-15 20:42:09 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.82%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n < 4) return n;
+        
+        int curr = 0;
+        int prev1 = 1;
+        int prev2 = 2;
+
+        for (int i = 3; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+
+        return curr;
+    }
+}
