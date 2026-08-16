830394f25c746abf456f41388b321a0f8907b16b
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-08-16 15:13:00 +0530

    Time: 0 ms (100%), Space: 42.7 MB (75.71%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int dp[] = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i = 2; i <= n; i++) {
+            int pick = nums[i - 1] + dp[i - 2];
+            int unpick = dp[i - 1];
+
+            dp[i] = Math.max(pick, unpick);
+        }
+        return dp[n];
+    }
+}
