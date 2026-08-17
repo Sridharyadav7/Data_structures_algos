ab4ca8990eb681f42c6a94b70b46e451b63eb365
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-08-17 21:50:09 +0530

    Time: 20 ms (80.91%), Space: 188.4 MB (22.77%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -17,13 +17,15 @@ class Solution {
         if(dp[prevInd][ind] != null) {
             return dp[prevInd][ind];
         }
-        boolean res = false;
+        
         for(int i = -1; i < 2; i++) {
             int val = stones[ind] - stones[prevInd] + i;
             if(map.containsKey(stones[ind] + val) && map.get(stones[ind] + val) > ind) {
-                res = res || recurse(n, stones, ind, map.get(stones[ind] + val), map, dp);
+                if (recurse(n, stones, ind, map.get(stones[ind] + val), map, dp)) {
+                    return dp[prevInd][ind] = true;
+                }
             }
         }
-        return dp[prevInd][ind] = res;
+        return dp[prevInd][ind] = false;
     }
 }
