73ccb76d4eff80664106d680ee2e6a510095cae0
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-09-09 20:39:56 +0530

    Time: 1 ms (99.93%), Space: 45.4 MB (62.26%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,35 +1,36 @@
 class Solution {
-    int sum = 0;
-    int dp[][] = new int[201][201];
+    int[][] dp;
 
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int sum =  triangle.get(0).get(0);
 
-        for (int arr[] : dp) {
+        dp = new int[n][n];
+
+        for (int[] arr : dp) {
             Arrays.fill(arr, Integer.MAX_VALUE);
         }
 
-        if (n == 1) {
-            return sum;
-        }
-        else {
-            return sum + find(n, triangle, 1, 0);
-        } 
+        return find(triangle, 0, 0);
     }
-    public int find(int n, List<List<Integer>> triangle, int row, int ind) {
 
-        if (row == n) {
-            return 0;
+    public int find(List<List<Integer>> triangle, int row, int ind) {
+
+        // Last row
+        if (row == triangle.size() - 1) {
+            return triangle.get(row).get(ind);
         }
 
+        // Already calculated
         if (dp[row][ind] != Integer.MAX_VALUE) {
             return dp[row][ind];
         }
 
-        int firstInd = triangle.get(row).get(ind) + find(n, triangle, row + 1, ind);
-        int secInd = triangle.get(row).get(ind + 1) + find(n, triangle, row + 1, ind + 1);
+        int current = triangle.get(row).get(ind);
 
-        return dp[row][ind] = Math.min(firstInd, secInd);
+        int first = current + find(triangle, row + 1, ind);
+
+        int second = current + find(triangle, row + 1, ind + 1);
+
+        return dp[row][ind] = Math.min(first, second);
     }
 }
