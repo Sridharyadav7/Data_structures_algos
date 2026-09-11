99d0d7a82e971333a325780d03d7808c9309f668
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-09-11 12:49:21 +0530

    Time: 442 ms (5.09%), Space: 237 MB (20.9%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,30 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp, 1);
-        int ans = 1;
+    int n;
+    int dp[][];
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if(nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[j] + 1, dp[i]);
-                }
-            }
-            ans = Math.max(ans, dp[i]);
+    public int lengthOfLIS(int[] nums) {
+        n = nums.length;
+        dp = new int[n+1][n+1];
+        for (int arr[]: dp) {
+            Arrays.fill(arr, -1);
+        }
+        return find(nums, 0, -1);
+    }
+    public int find(int nums[], int i, int prevInd) {
+        if (i == n) {
+            return 0;
         }
 
-        return ans;
+        if (dp[i][prevInd + 1] != -1) {
+            return dp[i][prevInd + 1];
+        }
+
+        int pick = 0;
+        if (prevInd == -1 || nums[i] > nums[prevInd]) {
+            pick = 1 + find(nums, i+1, i);
+        }
+
+        int unpick = find(nums, i+1, prevInd);
+        return dp[i][prevInd + 1] = Math.max(pick, unpick);
     }
 }
