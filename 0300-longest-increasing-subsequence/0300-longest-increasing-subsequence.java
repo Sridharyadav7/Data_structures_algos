ada3db1ee50971d6a2f4746c3bfd1b3b6b299a3b
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-09-11 12:56:31 +0530

    Time: 53 ms (32.83%), Space: 46.1 MB (70.58%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,30 +1,38 @@
 class Solution {
     int n;
-    int dp[][];
+    // int dp[][];
 
     public int lengthOfLIS(int[] nums) {
         n = nums.length;
-        dp = new int[n+1][n+1];
-        for (int arr[]: dp) {
-            Arrays.fill(arr, -1);
+        int dp[] = new int[n+1];
+        int ans = 1;
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] > nums[i]) {
+                    dp[j] = Math.max(dp[j], 1 + dp[i]);
+                    ans = Math.max(ans, dp[j]);
+                }
+            }
         }
-        return find(nums, 0, -1);
+        return ans;
     }
-    public int find(int nums[], int i, int prevInd) {
-        if (i == n) {
-            return 0;
-        }
+    // public int find(int nums[], int i, int prevInd) {
+    //     if (i == n) {
+    //         return 0;
+    //     }
 
-        if (dp[i][prevInd + 1] != -1) {
-            return dp[i][prevInd + 1];
-        }
+    //     if (dp[i][prevInd + 1] != -1) {
+    //         return dp[i][prevInd + 1];
+    //     }
 
-        int pick = 0;
-        if (prevInd == -1 || nums[i] > nums[prevInd]) {
-            pick = 1 + find(nums, i+1, i);
-        }
+    //     int pick = 0;
+    //     if (prevInd == -1 || nums[i] > nums[prevInd]) {
+    //         pick = 1 + find(nums, i+1, i);
+    //     }
 
-        int unpick = find(nums, i+1, prevInd);
-        return dp[i][prevInd + 1] = Math.max(pick, unpick);
-    }
+    //     int unpick = find(nums, i+1, prevInd);
+    //     return dp[i][prevInd + 1] = Math.max(pick, unpick);
+    // }
 }
