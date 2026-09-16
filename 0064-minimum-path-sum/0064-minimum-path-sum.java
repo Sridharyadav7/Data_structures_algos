b668a0d2f76700d633f60b49f360084d5bf35d7a
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-09-16 19:30:08 +0530

    Time: 7 ms (9.52%), Space: 50.4 MB (39.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -8,9 +8,29 @@ class Solution {
         n = grid[0].length;
         dp = new int[m+1][n+1];
         for (int arr[]: dp) {
-            Arrays.fill(arr, -1);
+            Arrays.fill(arr, Integer.MAX_VALUE);
         }
-        return solve(grid, 0, 0);
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1) {
+                    dp[i][j] = grid[i][j];
+                }
+                else {
+                    int down = Integer.MAX_VALUE;
+                    int right = Integer.MAX_VALUE;
+
+                    if (i < m - 1) {
+                        down = grid[i][j] + dp[i+1][j];
+                    }
+                    if (j < n - 1) {
+                        right = grid[i][j] + dp[i][j+1];
+                    }
+                    dp[i][j] = Math.min(down, right);
+                }
+            }
+        }
+        return dp[0][0];
     }
     public int solve(int[][] grid, int i, int j) {
         if (i == m - 1 && j == n - 1) {
