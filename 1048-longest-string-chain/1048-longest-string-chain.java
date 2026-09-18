020efd82b725a001db602bf13d538b190469379c
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-09-18 19:58:36 +0530

    Time: 69 ms (21.71%), Space: 46.3 MB (64.78%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -2,41 +2,56 @@ class Solution {
     public int longestStrChain(String[] words) {
         int n = words.length;
         int ans = 1;
-        Arrays.sort(words, (a, b) -> a.length() - b.length());
-        int dp[] = new int[n];
+        int dp[] = new int[n+1];
         Arrays.fill(dp, 1);
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if(words[j].length() == words[i].length() - 1 && isPredecessor(words[j], words[i])) {
-                    dp[i] = Math.max(dp[j] + 1, dp[i]);
-                } 
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isPredecessor(words[i], words[j])) {
+                    dp[j] = Math.max(dp[j], 1 + dp[i]);
+                }
+                ans = Math.max(dp[j], ans);
             }
-            ans = Math.max(ans, dp[i]);
         }
         return ans;
     }
 
-    public boolean isPredecessor(String s1, String s2) {
-        int i;
-        int j;
-        i = j = 0;
-        int skipped = 0;
+    public boolean isPredecessor(String word1, String word2) {
+        String s1;
+        String s2;
+
+        if (word1.length() < word2.length()) {
+            s1 = word1;
+            s2 = word2;
+        }
+        else {
+            s1 = word2;
+            s2 = word1;
+        }
+
+        if (s1.length() != s2.length() - 1) {
+            return false;
+        }
+
+        int diff = 0;
+        int i = 0;
+        int j = 0;
 
         while (i < s1.length() && j < s2.length()) {
-
             if (s1.charAt(i) == s2.charAt(j)) {
                 i++;
                 j++;
             }
-            else if (skipped < 1) {
-                j++;
-                skipped++;
-            }
             else {
-                return false;
+                diff++;
+                j++;
             }
         }
-        return true;
+        if (j == s2.length() - 1) {
+            diff++;
+        }
+        return diff == 1 ? true : false;
     }
 }
