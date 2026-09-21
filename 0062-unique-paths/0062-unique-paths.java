feab370bc1fe777e7931bb94f60d8d209aa8a954
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-09-21 19:50:54 +0530

    Time: 1 ms (13.27%), Space: 42.2 MB (28.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,16 +2,27 @@ class Solution {
     int M;
     int N;
     int dp[][];
-    
+
     public int uniquePaths(int m, int n) {
         M = m;
         N = n;
         dp = new int[M+1][N+1];
 
-        for (int arr[]: dp) {
-            Arrays.fill(arr, -1);
+        // for (int arr[]: dp) {
+        //     Arrays.fill(arr, -1);
+        // }
+
+        for (int i = M - 1; i >= 0; i--) {
+            for (int j = N - 1; j >= 0; j--) {
+                if (i == M - 1 && j == N - 1) {
+                    dp[i][j] = 1;
+                }
+                else {
+                    dp[i][j] = dp[i][j+1] + dp[i+1][j];
+                }
+            }
         }
-        return find(0, 0);
+        return dp[0][0];
     }
     public int find(int i, int j) {
         if (i >= M || j >= N) {
