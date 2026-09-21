6cb1fe317df27a53325944de4cc6ce01576cc21e
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-09-21 15:15:04 +0530

    Time: 24 ms (72.73%), Space: 249.6 MB (98.48%) - LeetHub

--- 3363-find-the-maximum-number-of-fruits-collected/3363-find-the-maximum-number-of-fruits-collected.java
+++ 3363-find-the-maximum-number-of-fruits-collected/3363-find-the-maximum-number-of-fruits-collected.java
@@ -7,9 +7,9 @@ class Solution {
         n = fruits.length;
         int ans = 0;
         dp2 = new int[n+1][n+1];
-        for (int arr[] : dp2) {
-            Arrays.fill(arr, -1);
-        }
+        // for (int arr[] : dp2) {
+        //     Arrays.fill(arr, -1);
+        // }
 
         dp3 = new int[n+1][n+1];
         for (int arr[] : dp3) {
@@ -17,7 +17,7 @@ class Solution {
         }
 
         ans = child1(fruits);
-        ans += child2(fruits, 0, n-1);
+        ans += child2(fruits);
         ans += child3(fruits, n-1, 0);
 
         return ans;
@@ -32,30 +32,41 @@ class Solution {
 
         return cnt;
     }
-    public int child2(int[][] fruits, int i, int j) {
+    public int child2(int[][] fruits) {
 
-        if (i >= n || j < 0 || j >= n) {
-            return 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == j || i > j) {
+                    continue;
+                }
+
+                if (i == n - 1 && j == n - 1) {
+                    continue;
+                }
+
+                int bottomLeft = 0;
+                int bottom = 0;
+                int bottomRight = 0;
+
+                if (i + 1 < n) {
+                    bottom = dp2[i + 1][j];
+
+                    if (j - 1 >= 0)
+                        bottomLeft = dp2[i + 1][j - 1];
+
+                    if (j + 1 < n)
+                        bottomRight = dp2[i + 1][j + 1];
+                }
+
+                dp2[i][j] = fruits[i][j] +
+                        Math.max(bottomLeft,
+                        Math.max(bottom, bottomRight));
+            }
         }
 
-        if (i == j || i > j) {
-            return 0;
-        }
-
-        if (i == n-1 && j == n-1) {
-            return 0;
-        }
-
-        if (dp2[i][j] != -1) {
-            return dp2[i][j];
-        }
-
-        int bottomLeft = fruits[i][j] + child2(fruits, i+1, j-1);
-        int bottom = fruits[i][j] + child2(fruits, i+1, j);
-        int bottomRight = fruits[i][j] + child2(fruits, i+1, j+1);
-
-        return dp2[i][j] = Math.max(bottomLeft, Math.max(bottom, bottomRight));
-    }
+    return dp2[0][n - 1];
+}
 
     public int child3(int[][] fruits, int i, int j) {
 
