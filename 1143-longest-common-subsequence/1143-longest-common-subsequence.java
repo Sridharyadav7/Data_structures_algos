b3a4d858925cce93f833e1b3dc1a55f43a32b847
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-09-22 20:40:43 +0530

    Time: 27 ms (23.13%), Space: 54.1 MB (36.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -8,11 +8,27 @@ class Solution {
         len2 = text2.length();
         dp = new int[len1+1][len2+1];
 
-        for (int arr[]: dp) {
-            Arrays.fill(arr, -1);
+        // for (int arr[]: dp) {
+        //     Arrays.fill(arr, -1);
+        // }
+
+        for (int i = len1 - 1; i >= 0; i--) {
+            for (int j = len2 - 1; j >= 0; j--) {
+                int pick = 0;
+                int moveI = 0;
+                int moveJ = 0;
+
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    pick = 1 + dp[i+1][j+1];
+                }
+                moveI = dp[i+1][j];
+                moveJ = dp[i][j+1];
+
+                dp[i][j] = Math.max(pick, Math.max(moveI, moveJ));
+            }
         }
 
-        return solve(text1, text2, 0, 0);
+        return dp[0][0];
     }
     public int solve(String s1, String s2, int i, int j) {
         if (i == len1 || j == len2) {
