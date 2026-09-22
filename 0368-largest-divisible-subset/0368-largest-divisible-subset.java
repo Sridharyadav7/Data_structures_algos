b6059d92f8eba0078ed4124866f04b5c748bd48c
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-09-22 19:56:19 +0530

    Time: 19 ms (21.78%), Space: 44.5 MB (99.18%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,55 +1,66 @@
 class Solution {
     int n;
-    int dp[][];
+    int dp[];
 
     public List<Integer> largestDivisibleSubset(int[] nums) {
         Arrays.sort(nums);
         n = nums.length;
-        dp = new int[n+1][n+1];
+        dp = new int[n];
+        Arrays.fill(dp, 1);
 
-        for (int arr[]: dp) {
-            Arrays.fill(arr, -1);
-        }
+        // for (int arr[]: dp) {
+        //     Arrays.fill(arr, -1);
+        // }
 
         List<Integer> ans = new ArrayList<>();
-        int i = 0;
-        int prevInd = -1;
+        int parent[] = new int[n];
+        Arrays.fill(parent, -1);
+        int maxInd = 0;
 
-        while (i < n) {
-            int best = find(nums, prevInd, i);
-            int pick = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] % nums[i] == 0) {
+                    if (1 + dp[i] > dp[j]) {
+                        dp[j] = 1 + dp[i];
+                        parent[j] = i;
 
-            boolean canPick = prevInd == -1 || nums[i] % nums[prevInd] == 0;
-
-            if (canPick) {
-                pick = 1 + find(nums, i, i+1);
+                        if (dp[j] > dp[maxInd]) {
+                            maxInd = j;
+                        }
+                    }
+                }
             }
+        }
 
-            if (canPick && pick == best) {
-                ans.add(nums[i]);
-                prevInd = i;
+        if (maxInd == 0) {
+            ans.add(nums[0]);
+        }
+        else {
+            while (maxInd >= 0) {
+                ans.add(nums[maxInd]);
+                maxInd = parent[maxInd];
             }
-            i++;
         }
         return ans;
+
     }
-    public int find(int nums[], int prevInd, int i) {
-        if (i == n) {
-            return 0;
-        }
+    // public int find(int nums[], int prevInd, int i) {
+    //     if (i == n) {
+    //         return 0;
+    //     }
 
-        if (dp[prevInd+1][i] != -1) {
-            return dp[prevInd+1][i];
-        }
+    //     if (dp[prevInd+1][i] != -1) {
+    //         return dp[prevInd+1][i];
+    //     }
 
-        int pick = 0;
-        int unpick = 0;
+    //     int pick = 0;
+    //     int unpick = 0;
 
-        if (prevInd == -1 || nums[i] % nums[prevInd] == 0) {
-            pick = 1 + find(nums, i, i+1);
-        }
-        unpick = find(nums, prevInd, i+1);
+    //     if (prevInd == -1 || nums[i] % nums[prevInd] == 0) {
+    //         pick = 1 + find(nums, i, i+1);
+    //     }
+    //     unpick = find(nums, prevInd, i+1);
 
-        return dp[prevInd+1][i] = Math.max(pick, unpick);
-    }
+    //     return dp[prevInd+1][i] = Math.max(pick, unpick);
+    // }
 }
