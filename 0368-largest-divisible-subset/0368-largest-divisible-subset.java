e8846f310eb6baf6cecc4ec15413d0e58273dbfd
Sridharyadav7 <105427547+Sridharyadav7@users.noreply.github.com>
2026-09-22 19:21:56 +0530

    Time: 43 ms (8%), Space: 51.2 MB (7.55%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,50 +1,55 @@
 class Solution {
+    int n;
+    int dp[][];
+
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int n = nums.length;
-
         Arrays.sort(nums);
+        n = nums.length;
+        dp = new int[n+1][n+1];
 
-        int[] dp = new int[n];
-        int[] parent = new int[n];
-
-        Arrays.fill(dp, 1);
-
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+        for (int arr[]: dp) {
+            Arrays.fill(arr, -1);
         }
 
-        int maxLen = 1;
-        int maxIndex = 0;
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int prevInd = -1;
 
-        for (int i = 0; i < n; i++) {
+        while (i < n) {
+            int best = find(nums, prevInd, i);
+            int pick = 0;
 
-            for (int j = 0; j < i; j++) {
+            boolean canPick = prevInd == -1 || nums[i] % nums[prevInd] == 0;
 
-                if (nums[i] % nums[j] == 0 &&
-                    dp[j] + 1 > dp[i]) {
-
-                    dp[i] = dp[j] + 1;
-                    parent[i] = j;
-                }
+            if (canPick) {
+                pick = 1 + find(nums, i, i+1);
             }
 
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                maxIndex = i;
+            if (canPick && pick == best) {
+                ans.add(nums[i]);
+                prevInd = i;
             }
+            i++;
+        }
+        return ans;
+    }
+    public int find(int nums[], int prevInd, int i) {
+        if (i == n) {
+            return 0;
         }
 
-        List<Integer> res = new ArrayList<>();
-
-        while (parent[maxIndex] != maxIndex) {
-            res.add(nums[maxIndex]);
-            maxIndex = parent[maxIndex];
+        if (dp[prevInd+1][i] != -1) {
+            return dp[prevInd+1][i];
         }
 
-        res.add(nums[maxIndex]);
+        int pick = 0;
+        int unpick = 0;
 
-        Collections.reverse(res);
+        if (prevInd == -1 || nums[i] % nums[prevInd] == 0) {
+            pick = 1 + find(nums, i, i+1);
+        }
+        unpick = find(nums, prevInd, i+1);
 
-        return res;
+        return dp[prevInd+1][i] = Math.max(pick, unpick);
     }
 }
